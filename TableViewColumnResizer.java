07d48d3c28e38339b389131001bffc00d5084577
Henryk Zschuppan <h.zschuppan@aol.com>
2025-04-07 05:35:44 +0200

    Update TableViewColumnResizer.java

--- TableViewColumnResizer.java
+++ TableViewColumnResizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Henryk Daniel Zschuppan, Mecklenburg-Vorpommern, Germany
+ * Copyright (c) April 2025 Henryk Daniel Zschuppan
  *
  * Developed in Mecklenburg-Vorpommern, Germany.
  *
